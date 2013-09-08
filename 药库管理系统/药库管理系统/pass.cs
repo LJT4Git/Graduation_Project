fc15ace9a2aaf778402cae1dfd1fd64d66ad12fc
lk <lvk618@gmail.com>
2013-09-08 15:21:10 +0800

    git commit -a

---
+++ 药库管理系统/药库管理系统/pass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace 药库管理系统
+{
+    public partial class pass : Form
+    {
+        public pass()
+        {
+            InitializeComponent();
+        }
+
+        private void pass_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            byte[] result = Encoding.Default.GetBytes(this.Psw.Text.Trim());    //Psw为输入密码的文本框
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(result);
+            this.textBox3.Text = BitConverter.ToString(output).Replace("-", "");  //textbox3为输出加密文本的文本框
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
